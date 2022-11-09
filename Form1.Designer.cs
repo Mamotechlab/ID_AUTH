d340c45a849ea7c334a76256d67da3b96d8ed013
Mamotechlab <113696286+Mamotechlab@users.noreply.github.com>
2022-11-09 22:53:41 +0530

    New FIle

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace ID_Auth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Next = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblmsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Next
+            // 
+            this.Next.BackColor = System.Drawing.SystemColors.Menu;
+            this.Next.Location = new System.Drawing.Point(688, 107);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(75, 23);
+            this.Next.TabIndex = 0;
+            this.Next.Text = "Next";
+            this.Next.UseVisualStyleBackColor = false;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(348, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email ID";
+            // 
+            // txtmail
+            // 
+            this.txtmail.BackColor = System.Drawing.SystemColors.Control;
+            this.txtmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmail.Location = new System.Drawing.Point(415, 107);
+            this.txtmail.Name = "txtmail";
+            this.txtmail.Size = new System.Drawing.Size(255, 23);
+            this.txtmail.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(435, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Login Details";
+            // 
+            // lblmsg
+            // 
+            this.lblmsg.AutoSize = true;
+            this.lblmsg.BackColor = System.Drawing.Color.Transparent;
+            this.lblmsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmsg.ForeColor = System.Drawing.Color.Tomato;
+            this.lblmsg.Location = new System.Drawing.Point(424, 147);
+            this.lblmsg.Name = "lblmsg";
+            this.lblmsg.Size = new System.Drawing.Size(0, 13);
+            this.lblmsg.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ID_Auth.Properties.Resources.logo2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblmsg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtmail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Next);
+            this.Name = "Form1";
+            this.Text = "Auth";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblmsg;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ID_Auth
+{
+    
+    public partial class Form1 : Form
+    {
+        public static DateTime Starttime;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Next_Click(object sender, EventArgs e)
+        {
+            try
+            {
+              
+                if (txtmail.Text.Trim()=="")
+                {
+                    MessageBox.Show("Enter Mail ID");
+                    return;
+                }
+                string mailid = txtmail.Text;
+                lblmsg.Text = "Authenticating .....";
+                // string DATA = @"{""email"":""pswinton@resiliantstage.com""}";
+                //string DATA = @"{""email"":""@"+mailid+@"""}";
+                string DATA = @"{""email"":""" + mailid + @"""}";
+
+                System.Net.HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://authproxy.resiliantstage.com/api/request");
+                request.Method = "POST";
+                request.ContentType = "application/json";
+                request.ContentLength = DATA.Length;
+                StreamWriter requestWriter = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.ASCII);
+                requestWriter.Write(DATA);
+                requestWriter.Close();
+
+                try
+                {
+                    WebResponse webResponse = request.GetResponse();
+                    Stream webStream = webResponse.GetResponseStream();
+                    StreamReader responseReader = new StreamReader(webStream);
+                    string response = responseReader.ReadToEnd();
+                    var result1 = JObject.Parse(response);
+                    string Transactionid = result1["data"]["TransactionId"].ToString();
+                    Console.Out.WriteLine(response);
+                    responseReader.Close();
+                    MessageBox.Show("Authetication Completed..Transactionid is "+ Transactionid);
+                    lblmsg.Text = "Authentication success .....";
+                    //Code to another process
+                    Starttime = DateTime.Now;
+                    if (Call_Next_Process(mailid, Transactionid)==true)
+                    {
+                        lblmsg.Text = "Verify successfully...";
+                        ProcessStartInfo info = new ProcessStartInfo(@"D:\windows-client\x64\Debug\SDP.exe");
+                        Process.Start(info);
+                        this.Close();
+                    }
+                    else
+                    {
+                        lblmsg.Text = "";
+                        MessageBox.Show("Verification failed");
+                    }
+
+
+
+
+
+
+
+
+                }
+                catch (Exception exc)
+                {
+                    lblmsg.Text = "";
+                    MessageBox.Show("Authetication failed");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public Boolean Call_Next_Process(string mailid,string transactionid)
+        {
+            lblmsg.Text = "Verifying .....";
+            string DATA = @"{""email"":""" + mailid + @"""}";
+
+            string fullfil = "false";
+
+         
+
+            do
+            {
+                try
+                {
+                    Thread.Sleep(5000);
+                    System.Net.HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://authproxy.resiliantstage.com/api/poll");
+                    request.Method = "POST";
+                    request.ContentType = "application/json";
+                    request.ContentLength = DATA.Length;
+                    StreamWriter requestWriter = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.ASCII);
+                    requestWriter.Write(DATA);
+                    requestWriter.Close();
+                    WebResponse webResponse = request.GetResponse();
+                    Stream webStream = webResponse.GetResponseStream();
+                    StreamReader responseReader = new StreamReader(webStream);
+                    string response = responseReader.ReadToEnd();
+                    var result1 = JObject.Parse(response);
+                    fullfil = result1["data"]["fulfilled"].ToString();
+                    Console.Out.WriteLine(response);
+                    responseReader.Close();
+
+                    if (result1["success"].ToString().ToUpper() =="FALSE")
+                    {
+                        lblmsg.Text = "";
+                        MessageBox.Show("Unauthorized user");
+                        return false;
+                    }
+
+                }
+                catch (Exception exn)
+                {
+                    lblmsg.Text = "";
+                    MessageBox.Show("Unauthorized user");
+                    return false;
+                }
+
+
+            } while (fullfil.ToString().ToUpper()=="FALSE" && Cal_Process_time() <= 2);
+
+
+            Boolean returntype = false;
+            if(fullfil.ToString().ToUpper()=="TRUE")
+            {
+                returntype = true;
+            }
+            else
+            {
+                returntype = false;
+            }
+                
+           
+
+           
+
+            return returntype;
+        }
+        public int Cal_Process_time()
+        {
+            DateTime date1 = Starttime;
+            DateTime date2 = DateTime.Now;
+            TimeSpan ts = date2 - date1;
+            return ts.Minutes;
+        }
+    }
+}
